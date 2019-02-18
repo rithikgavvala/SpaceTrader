7b0e72058270e5560c03c12be8357b37028c8ea4
vamsidesu5 <vamsidesu5@gmail.com>
2019-02-17 19:59:03 -0500

    player class edited, need to add gnat spaceship

--- app/src/main/java/com/example/codemonkeys/model/Player.java
+++ app/src/main/java/com/example/codemonkeys/model/Player.java
@@ -1,4 +1,21 @@
 package com.example.codemonkeys.model;
 
 public class Player {
+    private String characterName;
+    private int pilot;
+    private int fighter;
+    private int trader;
+    private int engineer;
+    private String difficulty;
+    private int creds;
+    // gnat spaceship needs to be created,
+    public Player(String name, int pil, int fight, int trad, int engine, String diff) {
+        characterName = name;
+        pilot = pil;
+        fighter = fight;
+        trader = trad;
+        engineer = engine;
+        difficulty = diff;
+        creds = 1000;
+    }
 }
