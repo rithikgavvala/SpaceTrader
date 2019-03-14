e418f50807969c6e3e9dc4115408d0dd0bfea656
vamsidesu5 <vamsidesu5@gmail.com>
2019-03-13 20:12:39 -0400

    solar system edit

--- app/src/main/java/com/example/codemonkeys/model/SolarSystem.java
+++ app/src/main/java/com/example/codemonkeys/model/SolarSystem.java
@@ -1,16 +1,19 @@
 package com.example.codemonkeys.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SolarSystem {
     String systemName;
     private Location location;
     private TechLevel techLevel;
+    private Resources resources;
     private List<TradeGood> listofResources;
 
-    public SolarSystem(String systemName, Location loc, TechLevel tl) {
+    public SolarSystem(String systemName, Location loc, TechLevel tl, Resources pol) {
         this.location = loc;
         this.techLevel = tl;
+        this.resources = pol;
         this.location = loc;
         listofResources = findGoodsAvailabletoSell();
     }
@@ -31,10 +34,16 @@ public class SolarSystem {
         this.techLevel = techLevel;
     }
 
-    private List<TradeGood> findGoodsAvailabletoSell(){
-
+    public Resources getResources() {
+        return resources;
     }
 
+    public void setResources(Resources resources) {
+        this.resources = resources;
+    }
+    private List<TradeGood> findGoodsAvailabletoSell(){
+        return new ArrayList<>();
+    }
 
     //TODO put a method here and add to constructor which allows you to list what items can be sold
 }
