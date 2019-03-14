20b3ceefdf271ed0b7f8b408b552d50b293fb1f9
saisahitib <saisahitib@gmail.com>
2019-03-14 18:46:21 -0400

    created SellAdapter created MarketViewModel created MarketInteractor added to Repository

--- app/src/main/java/com/example/codemonkeys/view/MarketActivity.java
+++ app/src/main/java/com/example/codemonkeys/view/MarketActivity.java
@@ -8,57 +8,13 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import com.example.codemonkeys.model.TradeGood;
 
 import com.example.codemonkeys.R;
-import com.example.codemonkeys.viewmodel.TransactionViewModel;
+import com.example.codemonkeys.viewmodel.MarketViewModel;
 
 
 public class MarketActivity extends AppCompatActivity {
-
-    /** a key for passing data */
-    public static final String STUDENT_DATA = "STUDENT_DATA";
-    /** an int for the request code */
-    private static final int EDIT_REQUEST = 5;
-    /** our data model */
-    private TransactionViewModel viewModel;
-    /** an adapter for the recycler view */
-    private SellAdapter adapter;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.sell_activity);
-
-        /*
-         Set up our recycler view by grabbing the layout for a single item
-         */
-        RecyclerView recyclerView = findViewById(R.id.sell_list);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setHasFixedSize(true);
-        // Setup the adapter for this recycler view
-        adapter = new SellAdapter();
-        recyclerView.setAdapter(adapter);
-
-        //grab our view model instance
-        viewModel = ViewModelProviders.of(this).get(TransactionViewModel.class);
-
-        Log.d("APP", viewModel.getStudents().toString());
-
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        adapter.setStudentList(viewModel.getStudents());
-
-        adapter.setOnStudentClickListener(new StudentAdapter.OnStudentClickListener() {
-            @Override
-            public void onStudentClicked(Student student) {
-                Intent intent = new Intent(ViewAllStudentsActivity.this, EditStudentActivity.class);
-                intent.putExtra(STUDENT_DATA, student);
-                startActivityForResult(intent, EDIT_REQUEST);
-            }
-        });
-    }
+    
 
 }
