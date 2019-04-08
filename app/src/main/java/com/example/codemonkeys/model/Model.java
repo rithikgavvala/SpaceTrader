66e7cec10cb82bc0a22c0dd15bc50298d1c1c9ac
Akhil Goel <akhil.goel.2000@gmail.com>
2019-04-08 14:09:23 -0400

    fixed gradle warning, added load method in model for deserializing from firebase. compile issue about saving serializedObject outside firebase method call.

--- app/src/main/java/com/example/codemonkeys/model/Model.java
+++ app/src/main/java/com/example/codemonkeys/model/Model.java
@@ -2,13 +2,18 @@ package com.example.codemonkeys.model;
 
 import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.util.HashMap;
@@ -60,6 +65,39 @@ public class Model {
         } catch (Exception e) {
              System.out.println(e);
              success = false;
+        }
+        return success;
+    }
+
+    public boolean load() {
+        boolean success = true;
+        try {
+            String serializedObject = "";
+            final FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference ref = database.getReference("players");
+            // Read from the database
+            ref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    serializedObject = dataSnapshot.getValue(String.class);
+                    Log.d("serial:",serializedObject);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                    Log.w("serial", "Failed to read value.", error.toException());
+                }
+            });
+            byte b[] = serializedObject.getBytes();
+            ByteArrayInputStream bi = new ByteArrayInputStream(b);
+            ObjectInputStream si = new ObjectInputStream(bi);
+            PlayerInteractor obj = (PlayerInteractor) si.readObject();
+        } catch (Exception e) {
+            System.out.println(e);
+            success = false;
         }
         return success;
     }
